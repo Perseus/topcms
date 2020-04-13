04e93defa370689feffeeecf133594bef597edbb
Anirudh Singh <anirudh@quizizz.com>
2020-04-13 21:43:56 +0530

    Clear application folder on BeforeInstall hook

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,4 +1,5 @@
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+rm -rf /home/ubuntu/topcms
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
