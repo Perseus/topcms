d9ad7802c8c17c73064d6c891598c9b46620c19d
Druthi <polisettydruthi.k@gmail.com>
2020-04-26 16:45:57 +0530

    Modularised schema

---
+++ api/src/graphql/schema/index.ts
@@ -0,0 +1,13 @@
+import { typeDefs as Commerce } from './commerce';
+import { typeDefs as Game } from './game';
+import { typeDefs as RootQueries } from './RootQueries';
+import { typeDefs as Site } from './site';
+import { typeDefs as User } from './user';
+
+export default [
+  Commerce,
+  Game,
+  RootQueries,
+  Site,
+  User
+];
